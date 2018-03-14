4dd3d158f334a73ecb98fbc6f0675a3149699204
Arda Ozcan <ardaoezcan@gmail.com>
2018-03-14 13:52:35 +0100

    changed

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+//fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=23540a6b8724dda669b1bd5ae46534fc")
+    //.then(response => response.json())
+    //.then((data) => {
+      //document.querySelectorAll("li").forEach(listItem => listItem.remove());
+      //data.words.forEach((word) => {
+        //const item = `<li>${word}</li>`;
+        //.insertAdjacentHTML("beforeend", item);
+      //});
+    //});
+//});
